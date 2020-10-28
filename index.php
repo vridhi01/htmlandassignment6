0c7ff130ea1e811632833ab82ccea5107092a750
deepak <deepak@excellencetechnologies.in>
2020-10-28 17:07:40 +0530

    assignment6

---
+++ index.php
@@ -0,0 +1,214 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Assignment6</title>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <link rel="stylesheet" href="index.css">
+   </head>
+   <body>
+    <div class="maindiv">
+           <!-- // navigation -->
+        <div class="navigation">
+            <div class="navigation1">
+                <div class="navlogo">
+                    <img src ="logo.png" />
+                </div>
+                <div class="navtext1">
+                    <p class="pstyle1">INGREDIENTS DIVISION</p>
+                </div>
+                <div class="leafimg">
+                    <img src ="leaf.png"/>
+                </div>
+                <div class="navsidetetxt">
+                    <p class="pstyle2">Starwest Sites</p>
+                    <div class="nav1ul">
+                        <a href="#" class="list1">Ingredients</a>
+                        <a href="#" class="list2">Retail </a>
+                        <a href="#" class="list3">Wholesale</a>
+                     </div>
+                     <div class="searchdiv">
+                    <input type ="text" Placeholder ="Search Products" class="inputsearchstyle"/>
+                    <button class="buttonstyle">Go</button>
+                     </div>
+                </div>
+            </div>
+            <div class="navigation2"> <!-- // navigation down  -->
+                <div class="nav2div1">
+                    <a class="diva" href="#">Home </a>
+                    <a class="diva" href="#">About Us</a>
+                    <a class="diva" href="#">Quality Assurance</a>
+                    <a class="diva" href="#">Sustainability</a>
+                    <a class="diva" href="#">Contact Us</a>
+                </div>
+                <div class="nav2div2">
+                    <p class="pstyle3">Ph:1-888-800-4372 </p>
+                </div>
+            </div>
+        </div>
+        </div>
+
+       <div class="maindiv2">
+             <!--  session div -->
+         <div class="centerdiv">
+           <!-- leftdiv start -->
+             <div class="leftsidediv">
+                <div class="l1div">
+                   <div class="products">PRODUCTS</div>
+                   <div class="productslist">
+                    <p class="proctlist1">-Botanicals</p>
+                    <p class="proctlist1">-Organic Botanicals</p>
+                    <p class="proctlist1">-Spices</p>
+                    <p class="proctlist1">-Organic Spices</p>
+                    <p class="proctlist1">-Spice Blends</p>
+                    <p class="proctlist1">-Teas</p>
+                    <p class="proctlist1">-Other Products</p>
+                    <img src="download1.png" class="imgstyle1" />
+                   </div>
+                </div>
+
+                <div class="l2div">
+                <div class="conten1">
+                    <h1 class="needassistance">NEED ASSISTANCE?</h1>
+                </div>
+                <img src ="girl.png"  class="imgstyle2"/>
+              
+                <div class="content2">
+                    <h1 class="needassistance">CALL:1-888-800-4372</h1>
+                </div>
+                </div>
+
+                
+                <div class="l3div">
+                 <div class="getupdate">GET UPDATES</div>
+                 <div class="getupdatelist">
+                     <p>Get automatic updates on our certifications </p>
+                     <p class="organicupdate"><img  src="squarebox.png"  class="imgstyle5"/>Organic Updates</p>
+                     <p class="Kosherupdate"><img  src="squarebox.png" class="imgstyle5"/>Kosher Updates</p>
+                     <input type="text" placeholder="Name" class="inputname"/>
+                     <input type="text" placeholder="Email Address" class="inutemailaddress"/>
+                     <button class="buttonstyle1">SIGN UP </button>
+                     <img src ="leftdiv.png" class="imgstyle3" />
+                 </div>
+                </div>
+                <div>
+                     <img src="logo.png" class="imgstyle4" />
+                 </div>
+             </div>
+              <!-- leftdiv finish-->
+               <!-- rightdiv start -->
+             <div class="rightsidediv">
+             <div class="container">
+             <div class="leftcontent">
+                    <h1><< 1 <span class="leftcontentspan">2</span> 3 >> </h1>
+                </div>
+
+                <img src="centerdiv.png"/>
+
+                <div class="content">
+                    <h1  class="imghstyle">Premium Ingredients Supplier of <span class="imgspan">Botanicals Herb </span> and 
+                    <span class="imgspan">Spices </span></h1>
+                </div>
+                
+                </div>
+                <!-- rightdiv1 start -->
+                <div class="rightdiv1">
+                     <h2 class="hstyle">Starwest Botanicals,Inc</h2>
+
+                   <div class="pstyle4">
+                    <img src="icon.png" class="imgstyle6" />
+                    <p class="innerp">Manufacture & Processor,with an emphasis on certified organic ingredients</p>
+                    </div>
+                    <div class="pstyle4">
+                    <img src="icon.png" class="imgstyle6" />
+                    <p class="innerp">Supply quantities from 25 pounds to full containers</p>
+                    </div>
+                    <div class="pstyle4">
+                    <img src="icon.png" class="imgstyle6" />
+                    <p class="innerp">Complete milling,blending & formulating services</p>
+                    </div>
+                    <div class="pstyle4">
+                    <img src="icon.png" class="imgstyle6" />
+                    <p class="innerp">Modern In-house Quality Control Laboratory,providing efficient microbiology,heavy metal & identify testing.</p>
+                    </div>
+                    <div class="pstyle4">
+                    <img src="icon.png" class="imgstyle6" />
+                    <p class="innerp">cGMP-& FDA-compliant manufacturing protocols are followed</p>
+                    </div>
+                    <div class="pstyle4">
+                    <img src="icon.png" class="imgstyle6" />
+                    <p class="innerp">Specification sheets,certificate of Analysis,Material Safety Data Sheets & other documents are provided</p>
+                    </div>
+                    <div class="pstyle4">
+                    <img src="icon.png" class="imgstyle6" />
+                    <p class="innerp">82,000 sq.ft warehouse enable us  to maintain a sizeable product inventory & maintain adequate stock levels.</p>
+                    </div>
+                 </div>
+                 <!-- rightdiv1 finish -->
+                 <!-- rightdiv2 start -->
+                 <div class="rightdiv2">
+                      <div class="latestnews">
+                          <div class="news">LATEST NEWS </div>
+                          <div class="newslist">
+                              <p>
+                                  And thus have these naked Nantucketers,these sea hermits,issuing from blah....<span class="spannews">Read More</span>
+                             </p>
+                             <p class="newspstyle">
+                                 "How have you bewitched them?"asked Tars Tarkas one afternoon,when...<span class="spannews"> Read More </span>
+                            </p>
+                          </div>
+                      </div>
+                      <div class="certificationdiv">
+                      <div class="certification">CERTIFICATIONS</div>
+                          <div class="certificationlist">
+                            <p class="certificationp">- 2009 AIB Audit Report</p>
+                            <p class="certificationp">- Organic System Plan</p>
+                            <p class="certificationp">- Organic Certificate of Compliance</p>
+                            <p class="certificationp">- Kosher Certificate</p>
+                          </div>
+                      </div>
+                 </div>
+                <!-- rightdiv2 finish -->
+                  <!-- footer -->
+                  <div class="footer">
+                    <div class="footerproducts">
+                        <p class="footerp">PRODUCTS</p>
+                        <p class="footerplist">Botanicals</p>
+                        <p class="footerplist">Organic Botanicals</p>
+                        <p class="footerplist">Spices</p>
+                        <p class="footerplist">Organic Spices</p>
+                    </div>
+                    <div class="footerinfo">
+                        <p class="footerp">INFO</p>
+                        <p>About Us</p>
+                        <p>Contact Us</p>
+                        <p>Certificate</p>
+                        <p>Services</p>
+                    </div>
+                    <div class="footerstarwestsites">
+                        <p class="footerp">STARWEST SITES </p>
+                        <p>Ingredients</p>
+                        <p>Retail</p>
+                        <p>Wholesale</p>
+                    </div>
+                    <div class="footerlinks">
+                        <p class="footerp">LINKS</p>
+                        <p>Disclaimer</p>
+                        <p>Privacy</p>
+                    </div>
+                    <div class="footerlast">
+                       <p>2010 StarwestHerb.com</p>
+                       <p>All Rights Reserved</p>
+                    </div>
+                   </div>
+                   <!-- footer finish -->
+              
+             </div>
+              <!-- rightdiv finish -->
+
+                 
+        </div>
+</div>
+   
+   </body>
+</html>
